5d64d32628ba47d2a11a52ed94a371f97435e79e
Sardor Madaminov <sardor0968@gmail.com>
2022-05-31 12:27:33 +0900

    login initialized

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
-import Message from "./Message";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
 
 function App() {
+  const user = useSelector(selecUser);
   return (
     <div className="app">
-      <h2> Here we build the Discord Clone</h2>
-      rfce
-      <Sidebar />
-      <Chat />
-      <Message />
+      {" "}
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="login">
+      {" "}
+      <h2> Login</h2>
+      <div className="login_logo">
+        <img
+          src="https://seeklogo.com/images/D/discord-black-logo-733DD6B9B0-seeklogo.com.png"
+          alt="discord"
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
