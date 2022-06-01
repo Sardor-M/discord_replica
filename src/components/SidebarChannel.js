5aa84aa379157cd42f1dee1c76c11f129900243f
Sardor Madaminov <sardor0968@gmail.com>
2022-06-01 13:31:09 +0900

    channel functionality

--- src/components/SidebarChannel.js
+++ src/components/SidebarChannel.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./SidebarChannel.css";
+import { useDispatch } from "react-redux";
+import { setChannelInfo } from "./features/appSlice";
 
 function SidebarChannel({ id, channelName }) {
+  const dispatch = useDispatch();
   return (
-    <div className="sidebarChannel">
-      <h4>
+    <div
+      className="sidebarChannel"
+      onClick={() =>
+        dispatch(
+          setChannelInfo({
+            channelId: id,
+            channelName: channelName,
+          })
+        )
+      }
+    >
+      <h3>
         <span className="sidebarChannel__hash">#</span>
         {channelName}
-      </h4>
+      </h3>
     </div>
   );
 }
