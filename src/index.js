64f6d07aceaa1e911a1efc32a1fa22d9d6fc9003
Sardor Madaminov <sardor0968@gmail.com>
2022-06-04 08:19:16 +0900

    some changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
 import store from "./app/store";
 import App from "./App";
 import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import ReactDOM from "react";
+import { Provider } from "react-redux";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
