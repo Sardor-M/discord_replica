ee4400f95df087999875ad6ca82558f423fb90d4
Sardor Madaminov <sardor0968@gmail.com>
2022-06-05 13:57:18 +0900

    serviceWorker added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,3 +14,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+serviceWorker.unregister();
